1248378205e9a8366d127e3d581cfba6a03e0d84
davidmarquescoder <davidmarques99@hotmail.com>
2023-05-20 23:13:40 -0300

    Configurações das urls, views e link css realizados

--- app/project/settings.py
+++ app/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'AMERICA/Sao_Paulo'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
