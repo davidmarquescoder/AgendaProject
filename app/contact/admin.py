5140b6a354a762e4f9499bea7e3e170618425200
davidmarquescoder <davidmarques99@hotmail.com>
2023-05-21 02:11:50 -0300

    Atualizando o admin e o models

--- app/contact/admin.py
+++ app/contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email',)
+    ordering = ('-id',) # Estou ordenando pelo id de forma decrescente
+    list_filter = ('created_date',) # Criando um filtro por data de criação
+    search_fields = ('first_name', 'last_name', 'email',) # Adicionando um campo de pesquisa para procurar contatos registrados
+    list_per_page = 10 # Setando quanto valores serão exibidos em cada página
+    list_max_show_all = 200 # Se clicar em 'mostrar tudo' ele exibirá no máximo 200 contatos
+    list_editable = ('first_name',)
+    list_display_links = ('phone', 'id',)
--- app/contact/models.py
+++ app/contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=250)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return (f'{self.first_name} {self.last_name} (id = {self.id})')
